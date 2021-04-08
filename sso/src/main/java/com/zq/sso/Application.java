3b588450a49b7c3503428478ed823e517be5aa2d
赵琦 <zhaoqi@changmu-inc.com>
2021-04-08 13:56:21 +0800

    base: nacos config

--- sso/src/main/java/com/zq/sso/Application.java
+++ sso/src/main/java/com/zq/sso/Application.java
@@ -4,8 +4,11 @@ package com.zq.sso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 @EnableDiscoveryClient
 @SpringBootApplication
+@RefreshScope
 public class Application {
 
     public static void main(String[] args) {
